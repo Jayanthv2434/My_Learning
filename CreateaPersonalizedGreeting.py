1cc99dbad5407fb631b6e161b019d9d02571324b
Jayanthv2434 <jayanth.v111@gmail.com>
2025-06-14 19:22:34 +0530

    Add files via upload

---
+++ CreateaPersonalizedGreeting.py
@@ -0,0 +1,3 @@
+First_name=str(input("Enter the first name"))
+Last_name=str(input ("Enter the last name"))
+print("Hello,"+First_name+Last_name,"! welcome to the python program")
---
+++ Perform Basic Mathematical Operations.py
@@ -0,0 +1,12 @@
+a =float(input("Enter the first number"))
+b=float(input("Enter the second number"))
+Addition =a+b
+Substraction=a-b
+multiplication=a*b
+print("Addition",Addition)
+print("Substraction",Substraction)
+print("multiplication",multiplication)
+if(b!=0):
+    division=a/b
+    print("division",division)
+else:print("0 can't divisible please enter the second number ")
